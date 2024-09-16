e0a6e676d77ea39a9535c4e956df854c7b711017
Daniel Vallejo <dandres.vallejo@udea.edu.co>
2024-09-15 21:28:20 -0500

    Actualización de los archivos

--- cobro_comisiones.py
+++ cobro_comisiones.py
@@ -1,8 +1,13 @@
+# Importamos las librerias requeridas para la implementaci�n del ejercicio
+
 import sqlite3
 import pandas as pd
+import openpyxl
 import smtplib
 from email.message import EmailMessage
-import openpyxl
+from dotenv import load_dotenv
+import os
+
 
 class Calculo_comisiones:
     
@@ -17,37 +22,37 @@ class Calculo_comisiones:
         FROM apicall AS A
         LEFT JOIN commerce AS C
         ON A.commerce_id = C.commerce_id
-        WHERE strftime('%Y-%m', A.date_api_call) = '{mes}'
+        WHERE strftime('%Y-%m', A.date_api_call) = '{mes}' 
         """
         self.cursor.execute(query)
         self.conn.commit()
 
-    def cobro_peticiones(self, empresa, peticiones_exitosas, peticiones_fallidas, peticiones_totales, precios, iva):
+    def cobro_peticiones(self, empresa, peticiones_exitosas, peticiones_fallidas, peticiones_totales, precios, limites,descuentos, porcentajes, iva):
         total = 0
         if empresa == 'Innovexa Solutions':
-            total = peticiones_exitosas * precios['Innovexa Solutions'] + peticiones_exitosas * precios['Innovexa Solutions'] * iva
+            total = peticiones_exitosas*precios['Innovexa Solutions'] + peticiones_exitosas*precios['Innovexa Solutions']*iva
         elif empresa == 'NexaTech Industries':
-            if peticiones_totales <= 10000:
-                total = peticiones_exitosas * precios['NexaTech Industries'][0] + peticiones_exitosas * precios['NexaTech Industries'][0] * iva
-            elif peticiones_totales <= 20000:
-                total = peticiones_exitosas * precios['NexaTech Industries'][1] + peticiones_exitosas * precios['NexaTech Industries'][1] * iva
+            if peticiones_totales <= limites['NexaTech Industries'][0]:
+                total = peticiones_exitosas*precios['NexaTech Industries'][0] + peticiones_exitosas*precios['NexaTech Industries'][0]*iva
+            elif peticiones_totales <= limites['NexaTech Industries'][1]:
+                total = peticiones_exitosas*precios['NexaTech Industries'][1] + peticiones_exitosas*precios['NexaTech Industries'][1]*iva
             else:
-                total = peticiones_exitosas * precios['NexaTech Industries'][2] + peticiones_exitosas * precios['NexaTech Industries'][2] * iva
+                total = peticiones_exitosas*precios['NexaTech Industries'][2] + peticiones_exitosas*precios['NexaTech Industries'][2]*iva
         elif empresa == 'QuantumLeap Inc.':
-            total = peticiones_exitosas * precios['QuantumLeap Inc.'] + peticiones_exitosas * precios['QuantumLeap Inc.'] * iva
+            total = peticiones_exitosas*precios['QuantumLeap Inc.'] + peticiones_exitosas*precios['QuantumLeap Inc.']*iva
         elif empresa == 'Zenith Corp.':
-            if peticiones_totales >= 0 and peticiones_totales <= 22000:
-                total = peticiones_exitosas * precios['Zenith Corp.'][0] + peticiones_exitosas * precios['Zenith Corp.'][0] * iva
-            elif peticiones_totales >= 22001:
-                total = peticiones_exitosas * precios['Zenith Corp.'][1] + peticiones_exitosas * precios['Zenith Corp.'][1] * iva
-            if peticiones_fallidas >= 6000:
-                total = total - total * 0.05
+            if peticiones_totales >= 0 and peticiones_totales <= limites['Zenith Corp.']:
+                total = peticiones_exitosas*precios['Zenith Corp.'][0] + peticiones_exitosas*precios['Zenith Corp.'][0]*iva
+            elif peticiones_totales > limites['Zenith Corp.']:
+                total = peticiones_exitosas*precios['Zenith Corp.'][1] + peticiones_exitosas*precios['Zenith Corp.'][1]*iva
+            if peticiones_fallidas >= descuentos['Zenith Corp.']:
+                total = total - total*porcentajes['Zenith Corp.']
         elif empresa == 'FusionWave Enterprises':
-            total = peticiones_exitosas * precios['FusionWave Enterprises'] + peticiones_exitosas * precios['FusionWave Enterprises'] * iva
-            if 2500 <= peticiones_fallidas <= 4500:
-                total = total - total * 0.05  # Descuento del 5% antes de IVA
-            elif peticiones_fallidas > 4501:
-                total = total - total * 0.08
+            total = peticiones_exitosas*precios['FusionWave Enterprises'] + peticiones_exitosas*precios['FusionWave Enterprises']*iva
+            if descuentos['FusionWave Enterprises'][0] <= peticiones_fallidas <= descuentos['FusionWave Enterprises'][1]:
+                total = total - total*porcentajes['FusionWave Enterprises'][0]  
+            elif peticiones_fallidas > descuentos['FusionWave Enterprises'][1]:
+                total = total - total*porcentajes['FusionWave Enterprises'][1]
         return total
 
     def calcular_comision(self, mes, precios, iva):
@@ -59,81 +64,105 @@ class Calculo_comisiones:
         resultado_tab = pd.crosstab(resultado['commerce_id'], resultado['ask_status'])
         resultado_tab['Total'] = resultado_tab['Successful'] + resultado_tab['Unsuccessful']
 
-        resultado_tab = resultado_tab.merge(commerce[['commerce_id', 'commerce_name', 'commerce_nit', 'commerce_email']], 
+        resultado_tab = resultado_tab.merge(commerce[['commerce_id', 'commerce_name', 'commerce_nit', 'commerce_email', 'commerce_status']], 
                                             left_on='commerce_id', right_on='commerce_id', how='left')
-        
+    
         resultado_tab['Cobro Total'] = resultado_tab.apply(
-            lambda row: self.cobro_peticiones(row['commerce_name'], row['Successful'], row['Unsuccessful'], row['Total'], precios, iva), axis=1)
-
-        resultado_tab['Fecha-Mes'] = mes
-        resultado_tab['Valor_comision'] = resultado_tab['Cobro Total'] / (1 + iva)
-        resultado_tab['Valor_iva'] = resultado_tab['Valor_comision'] * iva
+            lambda row: self.cobro_peticiones(row['commerce_name'], row['Successful'], row['Unsuccessful'], row['Total'], precios, limites, descuentos, porcentajes, iva) 
+            if row['commerce_status'] == 'Active' else 0, axis=1)
+    
+    
+        resultado_tab['Valor_comision'] = resultado_tab.apply(lambda row: row['Cobro Total'] / (1 + iva) if row['commerce_status'] == 'Active' else 0, axis=1)
+        resultado_tab['Valor_iva'] = resultado_tab.apply(lambda row: row['Valor_comision'] * iva if row['commerce_status'] == 'Active' else 0, axis=1)
         resultado_tab['Valor_Total'] = resultado_tab['Cobro Total']
-        
+    
+        resultado_tab['Fecha-Mes'] = mes
+    
         return resultado_tab
+
     
     def enviar_correo(self, archivo_excel, destinatario, remitente, contrasena):
-        # Crear el mensaje
+        
         msg = EmailMessage()
-        msg.set_content("Hola,\n\nAdjunto se encuentra el archivo con los resultados de las comisiones calculadas.\n\nSaludos,\nTu equipo")
+        msg.set_content(f"Hola, espero se encuentren muy bien,\n\nAdjunto el archivo donde se encuentra los resultados de las comisiones calculadas para cada empresa.\n\nSaludos")
         msg['Subject'] = 'Resultados de Comisiones'
         msg['From'] = remitente
         msg['To'] = destinatario
-
+    
         # Adjuntar el archivo Excel
         with open(archivo_excel, 'rb') as f:
             file_data = f.read()
             file_name = f.name
-
+    
         msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
-
+    
         # Conectar al servidor SMTP de Outlook y enviar el mensaje
         smtp_server = 'smtp.office365.com'
         smtp_port = 587
-
+    
         try:
             with smtplib.SMTP(smtp_server, smtp_port) as server:
-                server.starttls()  # Establecer una conexión segura
+                server.starttls()  
                 server.login(remitente, contrasena)
                 server.send_message(msg)
-                print("Correo electrónico enviado exitosamente.")
+                print(f"Correo electrónico enviado exitosamente a {destinatario}.")
         except Exception as e:
-            print(f"Error al enviar el correo: {e}")
-
-
+            print(f"Error al enviar el correo a {destinatario}: {e}")
+            
+            
+            
 # Conectar a la base de datos SQLite
-conn = sqlite3.connect('C:/Prueba_ecosistemas/database.sqlite')
 
-# Crear instancia de la clase
+conn = sqlite3.connect('C:/Prueba_ecosistemas/database.sqlite')
 calculo = Calculo_comisiones(conn)
 
-# Definir precios y el IVA
+# Generamos los diccionarios necesarios dentro de la automatización
 precios = {
     'Innovexa Solutions': 300,
-    'NexaTech Industries': [250,
-                            200, 
-                            170],
-    'QuantumLeap Inc.': 600,
-    'Zenith Corp.': [250,
-                     130],
+    'NexaTech Industries': [250,  # Asignar si hay entre 0 – 10.000 peticiones totales
+                            200,  # Asignar si hay entre 10.001 – 20.000 peticiones totales
+                            170], # Asignar si hay más de 20.001 peticiones totales
+    'QuantumLeap Inc.': 600, 
+    'Zenith Corp.': [250,  # Asignar si hay entre 0 – 22.000 peticiones totales
+                     130], # Asignar si hay más de 22.001 peticiones totales
     'FusionWave Enterprises': 300
 }
-iva = 0.19
+limites = {
+    'NexaTech Industries': [10000, 20000],  # Límites de peticiones totales
+    'Zenith Corp.': 22000  # Límites de peticiones totales
+}
+descuentos = {
+    'FusionWave Enterprises': [2500, 4500],  # Rango para descuento
+    'Zenith Corp.': 6000  # Valor para descuento
+}
+porcentajes = {
+    'FusionWave Enterprises': [0.05, 0.08],  # Rango para descuento
+    'Zenith Corp.': 0.05  # Valor para descuento
+}
 
-# Calcular comisiones para un mes dado
-mes = '2024-07'
+
+iva = 0.19 # Se añadde el valor del IVA colombiano
+mes = '2024-07' # Se añade el mes que se quiere desglosar, como ejemplo tomamos Julio del 2024
+
+
+# Calcular comisiones segpun la información suministrada anteriormente
+
 resultado_tab = calculo.calcular_comision(mes, precios, iva)
-
-# Guardar el DataFrame como archivo Excel Y definir las columnas necesarias según
-#lo estipulado en el correo
 
 archivo_excel = 'resultados_comisiones.xlsx'
 df_final = resultado_tab[['Fecha-Mes', 'commerce_name', 'commerce_nit', 'Valor_comision', 'Valor_iva', 'Valor_Total', 'commerce_email']]
 df_final = df_final.rename(columns={'commerce_name': 'Nombre', 'commerce_nit': 'Nit', 'commerce_email': 'Correo'})
 df_final.to_excel(archivo_excel, index=False)
 
-# Enviar el correo
-calculo.enviar_correo(archivo_excel, 'danielvallejo20@outlook.com', 'danielvallejo20@outlook.com', 'xafuna20')
+load_dotenv('C:\Prueba_ecosistemas\.env')
+correo_remitente = os.getenv('CORREO_REMITENTE')
+correo_ejecutor = os.getenv('CORREO_EJECUTOR')
+password = os.getenv('PASSWORD')
 
+# Guardar el DataFrame como archivo Excel general
+archivo_excel = 'resultados_comisiones.xlsx'
+df_final.to_excel(archivo_excel, index=False)
 
-df_final.to_excel('C:\Prueba_ecosistemas.xlsx')
+# Enviar el correo como ejecutor
+calculo.enviar_correo(archivo_excel, correo_ejecutor, correo_remitente , password)
+df_final.to_excel('C:\Prueba_ecosistemas\Resultados\df_final.xlsx')
